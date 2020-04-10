57f5ee528d706f20599202e36fc1cd84b9436198
wallandall <wallandall@gmail.com>
2020-04-10 12:46:03 +0200

    Added PrivateRoute component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import Navbar from './components/navbar/navbar.component';
 import Landing from './pages/landing/landing.component';
 import Login from './components/auth/login.component';
 import Register from './components/auth/register.component';
 import Alert from './components/alert/alert.component';
+import Dashboard from './components/dashboard/dasboard.component';
+import PrivateRoute from './components/routing/private-route.component';
 
 import { Provider } from 'react-redux';
 import store from './redux/store';
@@ -15,11 +18,11 @@ import './App.css';
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
-  store.dispatch(loadUser());
 }
+
 const App = () => {
   useEffect(() => {
-    store.dispatch(loadUser);
+    store.dispatch(loadUser());
   }, []);
   return (
     <Provider store={store}>
@@ -32,6 +35,7 @@ const App = () => {
             <Switch>
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
