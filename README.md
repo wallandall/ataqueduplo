ed51a7d0b6053e3d47c54d13813da8bee84628a9
wallandall <wallandall@gmail.com>
2020-04-13 21:52:16 +0200

    Prepare for Production

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Ataque Duplo is a BJJ Academy based in Hannover Germany, this application is dev
 
 ## Config
 
-To ensure this application runs correctly, ensure the following file is added to the project with the below configuration
-: `config/default.json` the file requires the following configuration:
+To ensure this application runs correctly, ensure the following files are added to the (default.json for Development and prod.json for Production ) project with the below configuration
+: `config/default.json` and `config/production.json`the file requires the following configuration:
 `{ "mongoURI": "YOUR_CONNECTION_STRING , "jwtSecret": "YOU_JWT_TOKEN" }`
+
+## Deployment
+
+Run `npm run build` from the client folder to build static files
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const connectDB = require('./config/bd');
+const path = require(path);
 
 const app = express();
 
@@ -8,11 +9,17 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Ataque Duplo API'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
